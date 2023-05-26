d39707a27ede0ce51cfdc318cfdccfe76e76b821
Your Name <ayushgandhi904@gmail.com>
2023-05-26 18:18:04 +0530

    model trainer

---
+++ src/pipeline/training_pipeline.py
@@ -0,0 +1,23 @@
+import os, sys
+import numpy as np
+import pandas as pd
+
+from src.logger import logging 
+from src.exception import CustomException
+from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
+from src.utils import evaluate_model, save_object
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data_path, test_data_path = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr, test_arr, _ = data_transformation.initaite_data_transformation(train_data_path, test_data_path)
+    
+    model_trainer = ModelTrainer()
+    model_trainer.initiate_model_training(train_arr, test_arr)
+    
+    
+    
--- src/utils.py
+++ src/utils.py
@@ -17,3 +17,30 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            #Model Training
+            model.fit(X_train, y_train)
+            
+            #Predicting Training data
+            # y_train_pred = model.predict(X_train)
+            
+            #Predicting Testing data
+            y_pred = model.predict(X_test)
+            
+            #Getting accuracy score
+            
+            # train_model_score = accuracy_score(y_train, y_train_pred)
+            test_model_score = accuracy_score(y_test, y_pred)
+            
+            report[list(models.keys)()[i]] = test_model_score
+            
+        return report
+    
+    except Exception as e:
+        logging.info("Error in evaluate model")
+        raise CustomException(e, sys)
